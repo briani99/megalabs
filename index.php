6b1380b9e3b07bd87df1f5439a70507e2807ddad
Brian Keenan <brian.keenan@sap.com>
2022-01-31 09:55:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once(“home.html”); ?>
+<html><head><meta charset="Windows-1252"></head><body>
+<img src="EUROFINS.PNG" WIDTH="400"><br><br><br>
+<h1>Number: 60458541 of 18/10/2021</h1>
+<h1>ID: 26742863G</h1>
+<h1>NAME: ARQUERO MUELA, YOLANDA</h1>
+<h1>
+SARS-CoV-2 RNA&nbsp;= Negative<br>
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="Ok.png" width="100">&nbsp;&nbsp;&nbsp;<img src="warning.png" width="100"><br>
+</h1></body></html>
